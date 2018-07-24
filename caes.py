3ce599af8c7a00c80446e3a03f6052e6e13aca88
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2018-07-24 16:28:07 +0200

    Sketch data structure and add params

--- caes.py
+++ caes.py
@@ -1,54 +1,49 @@
-from pyomo.environ import (ConcreteModel, Set, Param, Var, Constraint,
-                           Objective, Binary)
-from pyomo.environ import maximize
+import pandas as pd
+from pyomo.environ import *
 from pyomo.opt import SolverFactory
 
 
-# Data
-T = [1, 2, 3, 4]
-Cel = [20, 20, 20, 20]
-Pmax = [30, 30, 30, 30]
-
+# Load data
+sca = pd.read_csv('scalars.csv', index_col=0)
+seq = pd.read_csv('sequences.csv')
 
 # Create model
 m = ConcreteModel()
 
 # Sets
-m.T = Set(initialize=T)
+m.T = Set(initialize=seq['timestep'].values)
 
-# Params (loop over dict and work with setattr?)
-m.a = Param(m.T, initialize=a)
-m.b = Param(m.T, initialize=b)
-m.c = Param(m.T, initialize=c)
-m.d = Param(m.T, initialize=d)
-m.m_0 = Param(m.T, initialize=m_0)
-m.Eta_ex = Param(m.T, initialize=Eta_ex)
-m.exp_q_in_R = Param(m.T, initialize=exp_q_in_R)
-m.exp_q_in_T_0 = Param(m.T, initialize=exp_q_in_T_0)
-m.exp_q_in_cp = Param(m.T, initialize=exp_q_in_cp)
-m.cav_Pi_min =  Param(m.T, initialize=cav_Pi_min)
-m.cmp_P_max = Param(m.T, initialize=cmp_P_max)
-m.cmp_P_min = Param(m.T, initialize=cmp_P_min)
-m.exp_P_max = Param(m.T, initialize=exp_P_max)
-m.exp_P_min = Param(m.T, initialize=exp_P_min)
-
-
-m.C_el = Param(m.T, initialize=dict(zip(T, Cel)))
-
+# Params
+m.a = Param(m.T, initialize=sca.loc['a'].item())
+m.b = Param(m.T, initialize=sca.loc['b'].item())
+m.c = Param(m.T, initialize=sca.loc['c'].item())
+m.d = Param(m.T, initialize=sca.loc['d'].item())
+m.cmp_P_max = Param(m.T, initialize=sca.loc['cmp_P_max'].item())
+m.cmp_P_min = Param(m.T, initialize=sca.loc['cmp_P_min'].item())
+m.exp_P_max = Param(m.T, initialize=sca.loc['exp_P_max'].item())
+m.exp_P_min = Param(m.T, initialize=sca.loc['exp_P_min'].item())
+m.exp_q_in_Eta_ex = Param(m.T, initialize=sca.loc['exp_q_in_Eta_ex'].item())
+m.exp_q_in_R = Param(m.T, initialize=sca.loc['exp_q_in_R'].item())
+m.exp_q_in_T_0 = Param(m.T, initialize=sca.loc['exp_q_in_T_0'].item())
+m.exp_q_in_cp = Param(m.T, initialize=sca.loc['exp_q_in_cp'].item())
+m.cav_m_0 = Param(m.T, initialize=sca.loc['cav_m_0'].item())
+m.cav_Pi_min = Param(m.T, initialize=sca.loc['cav_Pi_min'].item())
+m.cav_Pi_max = Param(m.T, initialize=sca.loc['cav_Pi_max'].item())
+m.mkt_C_el = Param(m.T, initialize=dict(
+    zip(seq['timestep'].values, seq['mkt_C_el'].values)))
 
 # Variables
 m.P = Var(m.T, bounds=(0, 100))
-
-# Constraints
-def max_rule(model, t):
-    return(m.P[t] >= m.Pmax[t])
-m.max = Constraint(m.T, rule=max_rule)
-
 
 # Objective
 def obj_rule(model):
     return(sum(m.Cel[t]*m.P[t] for t in m.T))
 m.profit = Objective(sense=maximize, rule=obj_rule)
+
+# Constraints
+def max_rule(model, t):
+    return(m.P[t] >= m.Pmax[t])
+m.max = Constraint(m.T, rule=max_rule)
 
 # m.pprint()
 
