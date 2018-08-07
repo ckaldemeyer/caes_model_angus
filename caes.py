3ddad662902fd392ff5f5ac05a455c731c0e3a79
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2018-08-07 16:04:48 +0200

    Sketch new model

--- caes.py
+++ caes.py
@@ -22,22 +22,12 @@ m.T = po.Set(initialize=seq.index.values)
 m.a0 = po.Param(initialize=sca.loc['a0'].item())
 m.a = po.Param(initialize=sca.loc['a'].item())
 m.b = po.Param(initialize=sca.loc['b'].item())
-m.c0 = po.Param(initialize=sca.loc['c0'].item())
-m.c = po.Param(initialize=sca.loc['c'].item())
-m.d = po.Param(initialize=sca.loc['d'].item())
+m.c1 = po.Param(initialize=sca.loc['c1'].item())
+m.c2 = po.Param(initialize=sca.loc['c2'].item())
 m.cmp_P_max = po.Param(initialize=sca.loc['cmp_P_max'].item())
 m.cmp_P_min = po.Param(initialize=sca.loc['cmp_P_min'].item())
-m.cmp_m_max = po.Param(initialize=sca.loc['cmp_m_max'].item())
 m.exp_P_max = po.Param(initialize=sca.loc['exp_P_max'].item())
 m.exp_P_min = po.Param(initialize=sca.loc['exp_P_min'].item())
-m.exp_m_max = po.Param(initialize=sca.loc['exp_m_max'].item())
-m.exp_Eta_ex = po.Param(initialize=sca.loc['exp_Eta_ex'].item())
-m.exp_T_0 = po.Param(initialize=sca.loc['exp_T_0'].item())
-m.exp_R = po.Param(initialize=sca.loc['exp_R'].item())
-m.exp_ln = po.Param(initialize=sca.loc['exp_ln'].item())
-m.exp_cp = po.Param(initialize=sca.loc['exp_cp'].item())
-m.exp_Delta_T = po.Param(initialize=sca.loc['exp_Delta_T'].item())
-m.exp_Eta_comb = po.Param(initialize=sca.loc['exp_Eta_comb'].item())
 m.cav_m_0 = po.Param(initialize=sca.loc['cav_m_0'].item())
 m.cav_Pi_0 = po.Param(initialize=sca.loc['cav_Pi_0'].item())
 m.cav_Pi_min = po.Param(initialize=sca.loc['cav_Pi_min'].item())
@@ -51,20 +41,21 @@ m.mkt_C_fuel = po.Param(m.T, initialize=dict(zip(seq.index.values,
 # Add variables
 m.cmp_P = po.Var(m.T, domain=po.NonNegativeReals,
                  bounds=(0, sca.loc['cmp_P_max'].item()))
-m.cmp_m = po.Var(m.T, domain=po.NonNegativeReals)
 m.cmp_y = po.Var(m.T, domain=po.Binary)
+m.cmp_m = po.Var(m.T, domain=po.NonNegativeReals)
 m.cmp_z = po.Var(m.T, domain=po.NonNegativeReals,
                  bounds=(0, sca.loc['cav_Pi_max'].item()))
 m.exp_P = po.Var(m.T, domain=po.NonNegativeReals,
                  bounds=(0, sca.loc['exp_P_max'].item()))
-m.exp_Q = po.Var(m.T, domain=po.NonNegativeReals)
-m.exp_m = po.Var(m.T, domain=po.NonNegativeReals)
 m.exp_y = po.Var(m.T, domain=po.Binary)
-m.exp_z = po.Var(m.T, domain=po.NonNegativeReals,
-                 bounds=(0, sca.loc['cav_Pi_max'].item()))
+m.exp_m = po.Var(m.T, domain=po.NonNegativeReals)
+m.exp_Q = po.Var(m.T, domain=po.NonNegativeReals)
 m.cav_Pi = po.Var(m.T, domain=po.NonNegativeReals,
                   bounds=(sca.loc['cav_Pi_min'].item(),
                           sca.loc['cav_Pi_max'].item()))
+m.cav_Pi_o = po.Var(m.T, domain=po.NonNegativeReals,
+                    bounds=(sca.loc['cav_Pi_o_min'].item(),
+                            sca.loc['cav_Pi_o_max'].item()))
 
 
 # Add objective
@@ -72,8 +63,7 @@ m.cav_Pi = po.Var(m.T, domain=po.NonNegativeReals,
 m.profit_test = po.Objective(sense=po.minimize, rule=ru.obj_test)
 
 # Add constraints
-#m.cav_pi = po.Constraint(m.T, rule=ru.cav_pi)
-m.cav_pi_test = po.Constraint(m.T, rule=ru.cav_pi)
+m.cav_pi = po.Constraint(m.T, rule=ru.cav_pi)
 # m.cmp_z1 = po.Constraint(m.T, rule=ru.cmp_z1)
 # m.cmp_z2 = po.Constraint(m.T, rule=ru.cmp_z2)
 # m.cmp_z3 = po.Constraint(m.T, rule=ru.cmp_z3)
@@ -82,20 +72,11 @@ m.cav_pi_test = po.Constraint(m.T, rule=ru.cav_pi)
 m.cmp_area = po.Constraint(m.T, rule=ru.cmp_area)
 m.cmp_p_range_min = po.Constraint(m.T, rule=ru.cmp_p_range_min)
 m.cmp_p_range_max = po.Constraint(m.T, rule=ru.cmp_p_range_max)
-m.cmp_m_range_max = po.Constraint(m.T, rule=ru.cmp_m_range_max)
-# m.exp_z1 = po.Constraint(m.T, rule=ru.exp_z1)
-# m.exp_z2 = po.Constraint(m.T, rule=ru.exp_z2)
-# m.exp_z3 = po.Constraint(m.T, rule=ru.exp_z3)
-# m.exp_z4 = po.Constraint(m.T, rule=ru.exp_z4)
-# m.exp_z5 = po.Constraint(m.T, rule=ru.exp_z5)
-m.exp_area = po.Constraint(m.T, rule=ru.exp_area)
+m.exp_area1 = po.Constraint(m.T, rule=ru.exp_area1)
+m.exp_area2 = po.Constraint(m.T, rule=ru.exp_area2)
 m.exp_p_range_min = po.Constraint(m.T, rule=ru.exp_p_range_min)
 m.exp_p_range_max = po.Constraint(m.T, rule=ru.exp_p_range_max)
-m.exp_m_range_max = po.Constraint(m.T, rule=ru.exp_m_range_max)
 m.cmp_exp_excl = po.Constraint(m.T, rule=ru.cmp_exp_excl)
-#m.exp_fuel_1 = po.Constraint(m.T, rule=ru.exp_fuel_1)
-#m.exp_fuel_2 = po.Constraint(m.T, rule=ru.exp_fuel_2)
-m.test = po.Constraint(m.T, rule=ru.test)
 
 # Print model (select only a few timesteps)
 #m.pprint()
@@ -116,8 +97,7 @@ data = {'cmp_P': [m.cmp_P[t].value for t in m.T],
         'exp_y': [m.exp_y[t].value for t in m.T],
         'exp_Q': [m.exp_P[t].value for t in m.T],
         'cav_Pi': [m.cav_Pi[t].value for t in m.T],
-        'cmp_z': [m.cmp_z[t].value for t in m.T],
-        'exp_z': [m.exp_z[t].value for t in m.T],
+        'cav_Pi_o': [m.cav_Pi_o[t].value for t in m.T],
         'cmp_m': [m.cmp_m[t].value for t in m.T],
         'exp_m': [m.exp_m[t].value for t in m.T]}
 df = pd.DataFrame.from_dict(data)
--- rules.py
+++ rules.py
@@ -11,7 +11,7 @@ def obj(m):
 
 
 def obj_test(m):
-    expr = (sum(m.mkt_C_el[t] * m.cmp_P[t] +
+    expr = (sum(m.mkt_C_el[t] * m.cmp_P[t] -
                 m.mkt_C_el[t] * m.exp_P[t]
             for t in m.T))
     return expr
@@ -23,10 +23,6 @@ def cmp_p_range_min(m, t):
 
 def cmp_p_range_max(m, t):
     return(m.cmp_P[t] <= m.cmp_y[t] * m.cmp_P_max)
-
-
-def cmp_m_range_max(m, t):
-    return(m.cmp_m[t] <= m.cmp_y[t] * m.cmp_m_max)
 
 
 def cmp_area(m, t):
@@ -58,7 +54,7 @@ def cav_pi(m, t):
     if t == min(m.T):
         return(m.cav_Pi[t] == m.cav_Pi_0)
     else:
-        return(m.cav_Pi[t] == m.cav_Pi[t-1] +
+        return(m.cav_Pi[t] == m.cav_Pi_min + m.cav_Pi_o[t-1] +
                3600/m.cav_m_0 * (m.cmp_m[t] - m.exp_m[t]))
 
 
@@ -70,52 +66,13 @@ def exp_p_range_max(m, t):
     return(m.exp_P[t] <= m.exp_y[t] * m.exp_P_max)
 
 
-def exp_m_range_max(m, t):
-    return(m.exp_m[t] <= m.exp_y[t] * m.exp_m_max)
+def exp_area1(m, t):
+    return(m.exp_m[t] == m.exp_P[t] / m.c1)
 
 
-def exp_area(m, t):
-    return(m.exp_m[t] == (
-        m.c0 * m.exp_y[t] + m.c * m.exp_P[t] + m.d * m.cmp_z[t]))
-
-
-def exp_z1(m, t):
-    return(m.exp_z[t] >= m.cav_Pi_min * m.exp_y[t])
-
-
-def exp_z2(m, t):
-    return(m.exp_z[t] <= m.cav_Pi_max * m.exp_y[t])
-
-
-def exp_z3(m, t):
-    return(m.exp_z[t] >= m.cav_Pi[t] - (1 - m.exp_y[t]) * m.cav_Pi_max)
-
-
-def exp_z4(m, t):
-    return(m.exp_z[t] <= m.cav_Pi[t] - (1 - m.exp_y[t]) * m.cav_Pi_min)
-
-
-def exp_z5(m, t):
-    return(m.exp_z[t] <= m.cav_Pi[t] + (1 - m.exp_y[t]) * m.cav_Pi_max)
-
-
-def exp_fuel_1(m, t):
-    return(m.exp_Q[t] == (
-        m.exp_P[t] / m.exp_Eta_ex -
-        m.exp_m[t] * m.exp_T_0 * m.exp_R * m.exp_ln))
-
-
-def exp_fuel_2(m, t):
-    return(m.exp_Q[t] == (
-        m.exp_m[t] * m.exp_cp * m.exp_Delta_T / m.exp_Eta_comb))
+def exp_area2(m, t):
+    return(m.exp_Q[t] == m.exp_m[t] * m.c2)
 
 
 def cmp_exp_excl(m, t):
     return(m.cmp_y[t] + m.exp_y[t] <= 1)
-
-
-def test(m, t):
-    if t > 3 and t < 5:
-        return(m.cmp_y[t] == 0)
-    else:
-        return po.Constraint.Skip
